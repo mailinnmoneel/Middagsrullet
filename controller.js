4b0eb5727960c82c997570059a638bd776b8a952
mailinnmoneel <mailinn.kiste@gmail.com>
2021-06-23 19:20:55 +0200

    lekt med css

--- controller.js
+++ controller.js
@@ -1,1 +1,20 @@
+//Controller
+function randomFood() 
+{   
+        eatThisFood = food[Math.floor(Math.random() * food.length)];
 
+    updateView()
+}
+
+
+function addFood()
+{   
+    foodInputField = document.getElementById("foodSuggestions"); 
+    let newFood = foodInputField.value;
+
+    food.push(newFood);
+
+    alert('Ønsket er registrert!');
+
+    updateView()
+}

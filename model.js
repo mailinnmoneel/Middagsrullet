4576ad94ad2e63fa49295044fadfba38eeebb8ae
mailinnmoneel <mailinn.kiste@gmail.com>
2021-06-23 20:02:58 +0200

    ryddet i koden

---
+++ model.js
@@ -0,0 +1,10 @@
+
+let food = ['Kylling', 'Makrell', 'Pizza', 'Spaghetti', 'Tapas', 'Pannekaker', 'Wok', 'Burger', 'Biff', 'Kyllingsalat', ];
+
+
+
+let app = document.getElementById('app');
+let eatThisFood = 'Pizza';
+let iWantThisFood = '';
+var foodInputField;
+
--- view.js
+++ view.js
@@ -1,2 +1,17 @@
 
+updateView();
 
+function updateView() {
+
+    app.innerHTML=`
+    <div class="parent">
+        <h1> Usikker på hva du vil spise til middag i dag?</h1>
+        <button id="rulletDiv" onclick="randomFood()">Få et middagsforslag</button>
+        <div id="suggestionDiv">${eatThisFood}</div>
+        <div id="addField">
+            <input id="foodSuggestions" type="text">
+            <button id="registerButton" onclick="addFood()">Legg til egne middagsønsker</button>
+          </div>
+    </div>
+    `;
+}
